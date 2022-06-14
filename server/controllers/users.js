b55990368a82740dd4bba91566862906408b596e
DillonTodd <98577108+DillonTodd@users.noreply.github.com>
2022-06-13 23:04:35 -0500

    add the controller functions

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,1 +1,43 @@
+import { v4 as uuidv4 } from "uuid";
 
+let users = [];
+
+export const getUsers = (req, res) => {
+    res.send(users);
+}
+
+export const getUser = (req, res) => {
+    const { id } = req.params;
+
+    const userInstance = users.find((user) => user.id === id);
+
+    res.send(userInstance);
+}
+
+export const createUser = (req, res) => {
+    const user = req.body;
+
+    user.push({...user, id: uuidv4()});
+
+    res.send('User successfully created!');
+}
+
+export const updateUser = (req, res) => {
+    const { id } = req.params;
+
+    let userInstance = users.find((user) => user.id === id);
+
+    userInstance.firstName = req.body.firstName;
+    userInstance.lastName = req.body.lastName;
+    userInstance.age = req.body.age;
+
+    res.send('User successfully updated!');
+}
+
+export const deleteUser = (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id !== id);
+
+    res.send('User successfully deleted!');
+}
